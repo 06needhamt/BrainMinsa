22ed937ede043620f3a0552591d8f42bd8f48428
cnicholas63 <cnicholas63@gmail.com>
2016-01-13 10:11:49 +0000

    Improved answer entry routines

--- Boiler Plate/getAnswer.c
+++ Boiler Plate/getAnswer.c
@@ -6,19 +6,14 @@
 #include "String_Functions.h"
 
 
-void joystickLeft();
-void joystickRight();
-void joystickUp();
-void joystickDown();
-void acceptPressed();
+void joystickLeft(void);
+void joystickRight(void);
+void joystickUp(void);
+void joystickDown(void);
+void acceptPressed(void);
 
-// #defines for switch case
-#define LEFT 1
-#define RIGHT 2
-#define UP 3
-#define DOWN 4
-#define ACCEPT 5
-
+// define letter place holder as a . (dot)
+#define LETTER_PLACEHOLDER '.'
 
 
 
@@ -27,31 +22,48 @@ static struct entryDetails answerDetails;
 static int getAnswerState = 0;
 static int length = 0;
 static int margin = 0;
-static int charsetIndex = 0;
+
 
 char *initialiseGetAnswer(char *answer, int len) {
 	int t;
+	int position;
+  unsigned char letters[2] = {'\0', '\0'}; // Short string used to display letters
 	
 	if(getAnswerState == 1)  // Get answer is is already functioning
 		return ""; // <<<<<<<<<<<<<<<<< fix this
 	
 	getAnswerState = 1; // Signal getAnswerState is active
 	
-	for(t = 0; t < len; t++)
-		answerDetails.answerString[t] = 'Q'; // Fill initial answer string with nulls
+	answerDetails.letterIndex = 0; // index first character of answer string
 	
-	answerDetails.answerString[t] = 0; // Null terminate answerDetails
+	for(t = 0; t < len; t++)
+		answerDetails.answerString[t] = LETTER_PLACEHOLDER; // Fill initial answer string with PLACE_HOLDER
+	answerDetails.answerString[t] = '\0';  // Null terminate answerDetails
+	
 	
 	length = len; // Remember the length of the answer
 	
 	margin= (20 - length) / 2; // The width of the left margin for the answer
 	
-	// Display the empty answer place holders
-	for(t = 0; t < length; t++) {
-		GLCD_DisplayString(5, margin + t, __FI, ".");
-	}	
+	// Display the empty answer place holders with cursor highlighted
+	position = margin + answerDetails.letterIndex;
+	letters[0] = LETTER_PLACEHOLDER;
+	GLCD_SetBackColor(Red);
+	GLCD_SetTextColor(White);
+	GLCD_DisplayString(5, position, __FI, letters);
 	
-	//getAnswerState = 0; // No longet in getAnswer
+	// Reset default text colours
+	GLCD_SetBackColor(Blue);
+	GLCD_SetTextColor(White);
+	
+	for(t = 1; t < length; t++) 
+		GLCD_DisplayString(5, position + t, __FI, letters);
+	
+	
+	
+	
+	//GLCD_DisplayString(5, margin + t, __FI, ".");
+	
 	
 	return answer; // Return the address of the answer string
 	
@@ -85,14 +97,31 @@ void inputAnswer(int input) {
 // User pressed LEFT on joystick
 void joystickLeft() {
 	int position = 0;
+	unsigned char letters[2] = {'\0', '\0'}; // Short string used to display letters
 
+	// Display last letter using normal colours
+	position = margin + answerDetails.letterIndex;
+	letters[0] = answerDetails.answerString[answerDetails.letterIndex];
+	GLCD_SetBackColor(Blue);
+	GLCD_SetTextColor(White);
+	GLCD_DisplayString(5, position, __FI, letters);
+	
+	// Move the cursor left
 	if(--answerDetails.letterIndex < 0) {
 		answerDetails.letterIndex = length -1;
 	}
 	
+	// Display the current letter using highlight colours
 	position = margin + answerDetails.letterIndex;
-		
-	GLCD_DisplayString(6, position, __FI, "^");
+	letters[0] = answerDetails.answerString[answerDetails.letterIndex];
+	GLCD_SetBackColor(Red);
+	GLCD_SetTextColor(White);
+	GLCD_DisplayString(5, position, __FI, letters);
+	
+	// Reset default text colours
+	GLCD_SetBackColor(Blue);
+	GLCD_SetTextColor(White);	
+	
 	
 }
 
@@ -100,82 +129,102 @@ void joystickLeft() {
 // User pressed RIGHT
 void joystickRight() {
 	int position = 0;
-	
+	unsigned char letters[2] = {'\0', '\0'}; // Short string used to display letters
+
+	// Display last letter using normal colours
+	position = margin + answerDetails.letterIndex;
+	letters[0] = answerDetails.answerString[answerDetails.letterIndex];
+	GLCD_SetBackColor(Blue);
+	GLCD_SetTextColor(White);
+	GLCD_DisplayString(5, position, __FI, letters);
+
 	if(++answerDetails.letterIndex >= length) {
 		answerDetails.letterIndex = 0;
 	}
-	
+
+	// Display the current letter using highlight colours
 	position = margin + answerDetails.letterIndex;
-		
-	GLCD_DisplayString(6, position, __FI, "^");
+	letters[0] = answerDetails.answerString[answerDetails.letterIndex];
+	GLCD_SetBackColor(Red);
+	GLCD_SetTextColor(White);
+	GLCD_DisplayString(5, position, __FI, letters);
+	
+	// Reset default text colours
+	GLCD_SetBackColor(Blue);
+	GLCD_SetTextColor(White);	
+	
 }
 
 // User pressed UP
 void joystickUp() {
-	int t;
 	int index = 0;
 	int position = 0;
-	char newLetter;
-	char letter[2] = {'\0', '\0'};
+	//char newLetter;
+	unsigned char letters[2] = {'\0', '\0'}; // Short string used to display letters
 	
 	char character = answerDetails.answerString[answerDetails.letterIndex]; // Get the current letter from the answerString
 	
-	for(t = 0; t < CHARSET_LENGTH && character != OriginalCharSet[t]; t++) ; // Find the current character in the original set
+	for(index = 0; index < CHARSET_LENGTH && character != OriginalCharSet[index]; index++) ; // Find the current character in the original set
 		
-	newLetter = t; // Get the appropriate letter from the original character set
+	//newLetter = index; // Get the appropriate letter from the original character set
 
-
-	if(++newLetter >= CHARSET_LENGTH) // Gone past the last letter, return to the begining
-		newLetter = 0; 
-	
+	// This will also filter out the LETTER_PLACEHOLDER being outside the legal answer string range
+	if(++index >= CHARSET_LENGTH) // Gone past the last letter, return to the begining
+		index = 0; 
 		
-	answerDetails.answerString[answerDetails.letterIndex] = OriginalCharSet[newLetter];
+	answerDetails.answerString[answerDetails.letterIndex] = OriginalCharSet[index];
 	
-	letter[0] = OriginalCharSet[newLetter]; // Put the new letter into a printable string
-
-	position = margin + answerDetails.letterIndex;	
-
-	GLCD_DisplayString(5, position, __FI, letter);
+	// Display the letter using highlight colours
+	position = margin + answerDetails.letterIndex;
+	letters[0] = OriginalCharSet[index]; // Put the new letter into a printable string
+	GLCD_SetBackColor(Red);
+	GLCD_SetTextColor(White);
+	GLCD_DisplayString(5, position, __FI, letters);
 	
-	
+	// Reset default text colours
+	GLCD_SetBackColor(Blue);
+	GLCD_SetTextColor(White);	
 }
 
 // User pressed DOWN
 void joystickDown() {
-	int t;
 	int index = 0;
 	int position = 0;
-	char newLetter;
-	char letter[2] = {'\0', '\0'};
+	//char newLetter;
+	unsigned char letters[2] = {'\0', '\0'}; // Short string used to display letters
 	
 	char character = answerDetails.answerString[answerDetails.letterIndex]; // Get the current letter from the answerString
 	
-	if(character == 'A')
-		character = character;
-
-	for(t = 0; t < CHARSET_LENGTH && (character != OriginalCharSet[t]); t++) ; // Find the current character in the original set
-
-	newLetter = t; // Get the appropriate letter from the original character set
-
-
-	if(newLetter == 0) // Gone past the last letter, return to the begining
-		newLetter = CHARSET_LENGTH -1; 
-	else
-		newLetter--;
+	// Find the current character within the original character set. If found, characters index will be in index
+  for(index = 0; index < CHARSET_LENGTH && (character != OriginalCharSet[index]); index++) ; 
 	
-	answerDetails.answerString[answerDetails.letterIndex] = OriginalCharSet[newLetter];
+	if(index == 0 || index >= CHARSET_LENGTH) // At fist character in list OR character not found (must be LETTER_PLACEHOLDER)
+		index = CHARSET_LENGTH -1; // Set to last character of original character set
+	else 
+		index -= 1; // Set to previous letter from the original character set
 	
-	letter[0] = OriginalCharSet[newLetter]; // Put the new letter into a printable string
-
-	position = margin + answerDetails.letterIndex;	
-
-	GLCD_DisplayString(5, position, __FI, letter);
-
-
+	answerDetails.answerString[answerDetails.letterIndex] = OriginalCharSet[index];
+	
+	// Display the letter using highlight colours
+	position = margin + answerDetails.letterIndex;
+	letters[0] = OriginalCharSet[index]; // Put the new letter into a printable string
+	GLCD_SetBackColor(Red);
+	GLCD_SetTextColor(White);
+	GLCD_DisplayString(5, position, __FI, letters);
+	
+	// Reset default text colours
+	GLCD_SetBackColor(Blue);
+	GLCD_SetTextColor(White);
 }
 
 // User pressed ACCEPT button
 void acceptPressed() {
+	if(!getAnswerState) // Not expecting an answer so do nothing
+		return;
+	
+	
+	
+	
 	getAnswerState = 0; // Answer accepted no longer taking input
 }
 
